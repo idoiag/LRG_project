ec517045bbae1ace423025f3d336d759111bc8f1
Idoia <idoia.gomez-paramio@manchester.ac.uk>
2016-11-14 22:04:59 +0000

    Program  creates a csv and tab separated file with all coordinates

--- lrgext_v3.py
+++ lrgext_v3.py
@@ -13,13 +13,11 @@ data = 'LRG_1.xml'
 
 # change to  tree = ET.parse(filename + '.xml') once program is ready
 
-
-
 def get_structure(data):
     tree = ET.parse(data)
     root = tree.getroot()
-    fix_anno = tree.getroot()[0]
-    up_anno = tree.getroot()[1]
+    #fix_anno = tree.getroot()[0]
+    #up_anno = tree.getroot()[1]
     return(root, up_anno)
 
 def get_background(root):
@@ -63,7 +61,7 @@ def get_build_info(up_anno):
 def get_up_anno(data):
     """Get information about the strand information """
     
-    path_strand = "('./updatable_annotation/annotation_set')[2]"
+    #path_strand = "('./updatable_annotation/annotation_set')[2]"
     gene = root.find('updatable_annotation/annotation_set/lrg_locus').text
     print('Gene: ', gene)
 
@@ -73,8 +71,6 @@ def get_up_anno(data):
             
     return gene
 
-
-
 def get_exon_data(data):
     """Get information about the exon, including number of exons, exons coordinates 
     in the LRG system regarding the cdna, transcript and protein. Information is included 
@@ -82,56 +78,52 @@ def get_exon_data(data):
     
     exon_lst = root.findall('./fixed_annotation/transcript/exon')
     print('Exon count: ', len(exon_lst))
-    list_ex, list_ex_tr,list_ex_pt, list_ex  = [],[],[],[]
-    
+    #list_ex, list_ex_tr,list_ex_pt, list_ex  = [],[],[],[]
+    list_all_coord = []
     
     for exons in root.findall('./fixed_annotation/transcript/exon'):
         ex_num = exons.get('label')
-        list_ex.append(ex_num) 
         
-        for exons in root.findall('./fixed_annotation/transcript/exon/coordinates'):
+        for coord in exons.findall('coordinates'):
             
-            if (exons.get('coord_system').find("t1")!=-1):
-                start_ex_tr = exons.get('start')
-                end_ex_tr = exons.get('end')
-                list_ex_tr.append([ex_num, start_ex_tr, end_ex_tr])
+            if (coord.get('coord_system').find("t1")!=-1):
+                start_ex_tr = coord.get('start')
+                end_ex_tr = coord.get('end')
+                #list_ex_tr.append([ex_num, start_ex_tr, end_ex_tr])
             
-            elif (exons.get('coord_system').find("p1")!=-1):
-                start_ex_pt = exons.get('start')
-                end_ex_pt = exons.get('end')
-                list_ex_pt.append([ex_num, start_ex_pt, end_ex_pt])
+            elif (coord.get('coord_system').find("p1")!=-1):
+                start_ex_pt = coord.get('start')
+                end_ex_pt = coord.get('end')
+                #list_ex_pt.append([ex_num, start_ex_pt, end_ex_pt])
                 
             else:
-                start_ex = exons.get('start')
-                end_ex = exons.get('end')
-                list_ex.append([ex_num, start_ex, end_ex])
+                start_ex = coord.get('start')
+                end_ex = coord.get('end')
+                #list_ex.append([ex_num, start_ex, end_ex])
+        
+        list_all_coord.append([ex_num, start_ex, end_ex, start_ex_tr, end_ex_tr, start_ex_pt,end_ex_pt]  )
+        
+    print (list_all_coord)
                 
-    return (list_ex, list_ex_tr, list_ex_pt)
+    #return (list_ex, list_ex_tr, list_ex_pt)
+    return (list_all_coord)
      
-def printing():
-    """ Printing of functions' outputs """
-    
-    print ("\nExons coordinates: ", list_ex)
-    print ("\nTranscript coordinates: ", list_ex_tr)
-    print ("\nProtein coordinates: ", list_ex_pt)
-    
-    #db = open("db.txt","r+")
-    #db.write("Holaaaaa", list_ex)
-    #db.close()
+def output2file():
+    """ Creating csv and tab separate txt file """
+
+    db = open("LRG_coord.txt","w+")
+    for group in list_all_coord:   
+        db.write ("\t".join(group) + "\n")
+        
+    db_csv = open("LRG_coord.csv","w+")
+    for group in list_all_coord:   
+        db_csv.write (",".join(group) + "\n")
+        
+    db.close()
+    db_csv.close()
     
     return      
-db = open("db.txt","r+")
-db.write("Holaaaaa")
-db.close()
-
     
-# if transcript number differ between builds 37 and 38:
-# print("Do transcripts match between builds? ", True/False)
-
-# ouput all to .csv file or BED file
-# tree.write('output.txt')
-
-
 ##### TESTING #####
 """ Section dedicated to testing the running of the programm. It is subdivided into
 initial and secondary tests"""
@@ -155,11 +147,18 @@ def final_tests():
     return
     
 def strand_dir():
-    
+    """Check the strand direction and warn if in reverse.
+    Use LRG_571 for a forward strad example  """
     
     return
-### End of testing ###   
+
+def builds():
+    # if transcript number differ between builds 37 and 38:
+    # print("Do transcripts match between builds? ", True/False)
+    return
     
+### End of testing ###   
+   
 #### MAIN ####
 """ Main program running through different steps:
     1. Initial tests
@@ -167,7 +166,7 @@ def strand_dir():
     3. Get information about the different builds
     4. Get information about the strand
     5. Get information about the exon
-    6. Print in the standard output
+    6. Save exon, transcrip and protein coordinates to file
     7. Running of the final tests
 """
 
@@ -176,19 +175,25 @@ initial_tests()   # 1
 (schema, lrg_id,  hgnc_id, seq_source, transcript, cs, start_cs, end_cs, strand_cs) = get_background(root) # 3
 (coord, chro, NC_trans, gstart, gend) = get_build_info(up_anno) # 4
 (gene) = get_up_anno(data) # 5
-(list_ex, list_ex_tr, list_ex_pt)= get_exon_data(data) # 6
-printing() # 7
+(list_all_coord)= get_exon_data(data) # 6
+output2file() # 7
 final_tests()  # 8
 
 ### End of Main ###
+""" DONE
+Output: it creates a csv and tab separate text file
+
+"""
+
 
 """ to be done
 1. Compare builds
-2. Output, generate files
-3. Deal with issues, as having two transcripts
+2. Deal with issues, as having two transcripts
+
 """
 
 """ Outlook 
 1. Providing comand line input to access directly the webpage
 2. Adding new test using 'assert'
+3. Create a bed file
 """
