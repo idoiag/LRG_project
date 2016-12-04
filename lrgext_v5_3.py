915063a39c7298ed0322daf9cf05941f0e9fd6cd
Verity Fryer <verity.fryer@nhs.net>
2016-12-04 17:20:18 +0000

    Remove extraneous spaces

--- lrgext_v5_3.py
+++ lrgext_v5_3.py
@@ -5,47 +5,43 @@
 """
 
 import xml.etree.ElementTree as ET
-import os.path   
+import os.path
 # import sys
 """
-Usage: 
-lrgext extract build, gene, transcript, exon information from a file in LRG format 
+Usage: lrgext extract build, gene, transcript, exon information from a file in LRG format
 producing different output files:
     - cvs file
     - tab separated txt file
     - bed file
-
-"""    
-    
-
 """
 
+"""
 https://www.tutorialspoint.com/python/python_command_line_arguments.htm
 getopt.getopt(LRG, -l:, [long_options])
+
 """
 LRG = 'LRG_62'
 path = './LRGs/'
 data = path + LRG + '.xml'
 
-
 # ask user to input LRG name
 # filename = input("Enter LRG name: ")
-# change to  tree = ET.parse(filename + '.xml') once program is ready
+# change to tree = ET.parse(filename + '.xml') once program is ready
+# use sys.argv for user to enter filename at the command line. Need to import sys.
 
+"""Use the 'xml.etree.ElementTreee' to extract information from xml files"""
 def handle_xml(data):
-    """Use the 'xml.etree.ElementTreee' to extract inoformation from xml files"""
-    
     tree = ET.parse(data)
     root = tree.getroot()
     up_anno = tree.getroot()[1]
     return(root, up_anno)
 
+"""Get background information about the gene"""
 def get_background(root):
-    """Get background information about the gene """
-    
-    for lrg in root.findall ("."):  
-        schema = lrg.get('schema_version')  
-        
+
+    for lrg in root.findall ("."):
+        schema = lrg.get('schema_version')
+
     for fixed in root.findall("./fixed_annotation"):
         lrg_id = fixed.find('id').text
         hgnc_id = fixed.find ('hgnc_id').text
@@ -60,17 +56,14 @@ def get_background(root):
                 start_cs = coordinates.get('start')
                 end_cs = coordinates.get('end')
                 strand_cs = coordinates.get('strand')
-        
-        print (schema) 
-        print ( lrg_id,  hgnc_id, seq_source, transcript, cs, start_cs, end_cs, strand_cs)
-        return ( schema, lrg_id,  hgnc_id, seq_source, transcript, cs, start_cs, end_cs, strand_cs)
+
+        print (schema)
+        print (lrg_id,  hgnc_id, seq_source, transcript, cs, start_cs, end_cs, strand_cs)
+        return (schema, lrg_id,  hgnc_id, seq_source, transcript, cs, start_cs, end_cs, strand_cs)
 
 
 def get_build_info(up_anno):
-    """Get build information, including coordinates, chromosome, transcript,
-    and genomic start and end. It will provide "N/A", when protein coordinates are not
-    available"""
-    
+"""Get build information, including coordinates, chromosome, transcript and genomic start and end. It will provide "N/A", when protein coordinates are not available"""
     for annotation in up_anno[1].findall('mapping'):
         coord = annotation.get('coord_system')
         chro = annotation.get('other_name')
@@ -79,18 +72,15 @@ def get_build_info(up_anno):
         gend = annotation.get('other_end')
         print (coord, chro, NC_trans, gstart, gend)
         return (coord, chro, NC_trans, gstart, gend)
-        
-        ####VF CODE###
-        # Not sure if the differences between builds could be included here or 
-        # in a different function.
-        
-        ## Also to capture if difference in sequences occur in the intro/exon
-        
-            
+
+####VF CODE###
+        # Not sure if the differences between builds could be included here or in a different function. 
+        # Also to capture if difference in sequences occur in the intro/exon
+###
+
+"""Extract gene name (HGVS nomenclature) and tag strand as forward(+) or reverse(-)"""
 def get_gen_data(data):
-    """Extract gene name (HGVN nomenclature) and tag strand as 
-    forward(+) or reverse(-) """
-    
+
     # Extracting name of gene
     gene = root.find('updatable_annotation/annotation_set/lrg_locus').text
     print('Gene: ', gene)
@@ -98,154 +88,139 @@ def get_gen_data(data):
     # Determining the strand direction(e.g. 517 is "+")
     for annotation in root.findall('./updatable_annotation/annotation_set[@type="lrg"]/mapping[@type="main_assembly"]/mapping_span'):
         str_dir = annotation.attrib['strand']
-        
+
         # marking forward strand as "+" and negative as "-"
         if (str_dir == "1"):
             str_dir = "+"
         else:
             str_dir = "-"
         print (str_dir)
-            
+
     return (gene, str_dir)
-    
+"""Get information about the exon for the different transcripts, including number of exons, exons coordinates in the LRG system regarding the cdna, transcript and protein """
+
 def get_exon_data(data, gstart, gend, chro, str_dir):
-    """Get information about the exon for the different transcripts, including number of exons, exons coordinates 
-    in the LRG system regarding the cdna, transcript and protein """
-    
+
     trans_number = 0
     list_all_coord, list4bed = [], []
-    
-    # Loop to extract information when there is more than 1 transcript 
-    # (e.g.LRG_214 has two transcripts
+
+    # Loop to extract information when there is more than 1 transcript (e.g. LRG_214 has two transcripts
     for transcripts in root.findall('./fixed_annotation/transcript'):
         trans_number += 1
-     
+
         exon_lst = root.findall('./fixed_annotation/transcript/exon')
         print( "Transcript number: ", trans_number, 'Exon count: ', len(exon_lst))
-    
+
         for exons in transcripts.findall('exon'):
             ex_num = exons.get('label')
-            
+
             # Extracting transcript, protein and exon coordinates (in this order)
-             # Print "N/A" if protein coordinates are not available (e.g. LRG_292)
+            # Print "N/A" if protein coordinates are not available (e.g. LRG_292)
             start_ex_pt = "N/A"
             end_ex_pt = "N/A"
-            
+
             for coord in exons.findall('coordinates'):
                 if (coord.get('coord_system').find("t")!=-1):
                     start_ex_tr = coord.get('start')
                     end_ex_tr = coord.get('end')
-                
+
                 elif (coord.get('coord_system').find("p")!=-1):
                     start_ex_pt = coord.get('start')
                     end_ex_pt = coord.get('end')
-                    
+
                 elif (coord.get('coord_system').find("p")==-1) or (coord.get('coord_system').find("t" )==-1):
                     start_ex = coord.get('start')
                     end_ex = coord.get('end')
                     g_start_ex = start_ex + gstart
                     g_end_ex = end_ex + gend
-                
+
                 else:
-                    print ("problem when extrating exon info")
-                    
+                    print ("Problem extracting exon information")
+
             # Create list of coordinates
             list4bed.append([chro, g_start_ex, g_end_ex, str_dir, str(trans_number)])
             list_all_coord.append([str(trans_number), ex_num, start_ex, end_ex, start_ex_tr, end_ex_tr, start_ex_pt,end_ex_pt]  )
-    
-    # Preparing lists to be print in columns
-    for group in list_all_coord: 
-        print ("\t".join(group) + "\n")
-                
-    return (list_all_coord, list4bed)
-     
-def output2file(list_all_coord, list4bed):
-    """ Creating csv, a tab separate txt file with exon, transcripts and 
-    protein coordinates and a bed file"""
 
-    # Opening files to read and write. If not existing, it will create a new one
+    # Preparing lists to be print in columns
+    for group in list_all_coord:
+        print ("\t".join(group) + "\n")
+    return (list_all_coord, list4bed)
+
+""" Creating .csv, a comma separated text file with exon, transcripts and protein coordinates and a bed file"""
+def output2file(list_all_coord, list4bed):
+    # Open file in read/write format. If one doesn't exist, it  will create a new one
     db = open("./Outputs/LRG_coord.txt","w")
     db_csv = open("./Outputs/LRG_coord.csv","w")
     bed = open ("./Outputs/LRG_bed", "w")
-    
-    # Add headings to files
+
+    # Add headers to files
     headings = ["transcript","exon", "ex_start", "ex_end", "tr_start", "tr_end", "pt_start", "pt_end"]
     bed_headings = ["chr", "start", "end", "strand", "transcript" ]
-    
-    #Writting tab separated text file
-    db.write("\t".join(headings) + "\n") # writting headings
-    for group in list_all_coord:   
-        db.write ("\t".join(group) + "\n") # writting coordinates
-    
-    #Writting csv file
-    db_csv.write(",".join(headings) + "\n") # writting headings
-    for group in list_all_coord:   
-        db_csv.write (",".join(group) + "\n") # writting coordinates
-    
-    #Writting bed file
-    bed.write("\t".join(bed_headings) + "\n") # writting headings
-    for group in list4bed:   
+
+    # Writing tab separated text file
+    db.write("\t".join(headings) + "\n") # writing headings
+    for group in list_all_coord:
+        db.write ("\t".join(group) + "\n") # writing coordinates
+
+    # Writing csv file
+    db_csv.write(",".join(headings) + "\n") # writing headings
+    for group in list_all_coord:
+        db_csv.write (",".join(group) + "\n") # writing coordinates
+
+    # Writing bed file
+    bed.write("\t".join(bed_headings) + "\n") # writing headings
+    for group in list4bed:
         bed.write ("\t".join(group) + "\n")
-    
+
     # Closing files
     db.close()
     db_csv.close()
     bed.close()
-    
-    return         
-    
+    return
+
 def disclaimer():
-    print ( 
-    """Please cite this software as: 'Gomez-Paramio, I. and Fryer, V. (2016), 'lrgext', Software, 
+# This disclaimer is in triple quotes therefore won't print - is this because i needs to be in double quotes or because it needs to be amended first?
+    print ("""Please cite this software as: 'Gomez-Paramio, I. and Fryer, V. (2016), 'lrgext', Software, 
     Faculty of Medicine and Human Sciences, The University of Manchester.' or successor 
     references as defined by the authors.\n""")
-    
     return
-    
+
 ##### TESTING #####
 
 def initial_tests():
-    """ Run initial tests to check the software and file before execution"""
-    
+    """ Run initial tests to check the software and file before execution""" 
     if (os.path.isfile(data) == False) :
         print ("Data is not a readable file")
-    
+
     if (os.path.exists(data) == False):
         print ("Data does not exits")
-    
-    #### VF's code ####
+
+#### VF's code ####
     # check file is in xml format. If not, return error message "Not an xml file"
     # add try, except to close program if no LRG exists
-    
-    
     return
-    
-    
-def final_tests():  
-    """Tests run at the end of the program"""
-    
+
+"""Tests run at the end of the program"""
+def final_tests():
     if schema != "1.9":  # Checking for xml format
-        print ("""lrgext supports LRG xmls built using schema 1.9. Please" 
-        be aware of data incongruencies""")
+        print ("""lrgext supports LRG xmls built using schema 1.9. Please be aware of data incongruencies""")
     return
-    
+
 def strand_dir():
     """Check the strand direction and warn if in reverse.
-    Use LRG_571 for a forward strad example  """
+    Use LRG_571 for a forward strand example  """
     #### IDOIA CODE ####
-    
     return
 
 def builds():
-    
-    #### VF's code
+
+#### VF's code
     # if transcript number differ between builds 37 and 38:
     # print("Do transcripts match between builds? ", True/False)
-    
     return
-    
-### End of testing ###   
-   
+
+### End of testing ###
+
 #### MAIN information ####
 
 initial_tests()   # 1
@@ -253,10 +228,10 @@ initial_tests()   # 1
 (schema, lrg_id,  hgnc_id, seq_source, transcript, cs, start_cs, end_cs, strand_cs) = get_background(root) # 3
 (coord, chro, NC_trans, gstart, gend) = get_build_info(up_anno) # 4
 (gene, str_dir) = get_gen_data(data) # 5
-(list_all_coord, list4bed)= get_exon_data(data, gstart, gend, chro, str_dir) # 6
+(list_all_coord, list4bed) = get_exon_data(data, gstart, gend, chro, str_dir) # 6
 output2file(list_all_coord, list4bed) # 7
 disclaimer() # 8
-final_tests() # 9 
+final_tests() # 9
 
 
 ### End of Main ###
@@ -302,6 +277,5 @@ Main program running through different steps:
 1. Providing comand line input to access directly the files/webpage
 2. Adding new test using 'assert'
 3. Automatic creation of output file names
-
 
 """
